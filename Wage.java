5236210a5228374f67c44bdf0ff7e28dce730c51
unknown <rahulsurana37@gmail.com>
2020-06-22 16:17:09 +0530

    Added Switch case method

--- Wage.java
+++ Wage.java
@@ -5,12 +5,51 @@ public static void main(String[] args) {
 	int Wage_per_hour=20;
 	int Full_time_hrs=8;
 	int Part_time_hrs=4;
-	int fulltime=1;
-	int parttime=2;
+	double absent=0;
+	double fulltime=1;
+	double parttime=2;
 	int salary;
 	double random_Check=Math.floor(Math.random() * 10) % 2;
 	double random_Check_1=Math.floor(Math.random() * 10) % 3;
-	if (random_Check==is_Present) {
+	int random=(int)random_Check_1;
+			switch(random){
+				case 1:
+				System.out.println("Employee is present and is Full-time");
+				salary=Wage_per_hour*Full_time_hrs;
+				System.out.println("The full-time salary is "+salary);
+				break;
+				case 2:
+				System.out.println("Employee is present and is Part-time");
+				salary=Wage_per_hour*Part_time_hrs;
+				System.out.println("The Part time salary is "+salary);
+				break;
+				default:
+				System.out.println("Employee is absent");
+			}
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*if (random_Check==is_Present) {
 		if (random_Check_1==fulltime) {
 			System.out.println("Employee is present and is Full-time.");
 			salary=Wage_per_hour*Full_time_hrs;
@@ -26,4 +65,4 @@ public static void main(String[] args) {
 		System.out.println("Employee is absent");
 	}
 }
-}
+}*/
