940b6f6e4edbd2ceb7061458f25eab9841652d70
unknown <rahulsurana37@gmail.com>
2020-06-22 15:23:41 +0530

    Attendance is added.

--- Wage.java
+++ Wage.java
@@ -1,5 +1,15 @@
 class Wage{
 public static void main(String[] args) {
 	System.out.println("Welcome to JAVA exercise");
+
+int is_Present=1;
+	double random_Check=Math.floor(Math.random() * 10) % 2;
+	if (random_Check==is_Present) {
+		System.out.println("Employee is present");
+		
+	}
+	else{
+		System.out.println("Employee is absent");
+	}
 }
 }
