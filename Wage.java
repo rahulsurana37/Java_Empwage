bc9033fc5dcfca0360e29b9f8d96edf42ebc4eda
unknown <rahulsurana37@gmail.com>
2020-06-22 15:58:24 +0530

    Added part time salary

--- Wage.java
+++ Wage.java
@@ -3,13 +3,24 @@ public static void main(String[] args) {
 	System.out.println("Welcome to JAVA exercise");
 	int is_Present=1;
 	int Wage_per_hour=20;
-	int Full_time=8;
+	int Full_time_hrs=8;
+	int Part_time_hrs=4;
+	int fulltime=1;
+	int parttime=2;
 	int salary;
 	double random_Check=Math.floor(Math.random() * 10) % 2;
+	double random_Check_1=Math.floor(Math.random() * 10) % 3;
 	if (random_Check==is_Present) {
-		System.out.println("Employee is present");
-		salary=Wage_per_hour*Full_time;
-		System.out.println(salary);
+		if (random_Check_1==fulltime) {
+			System.out.println("Employee is present and is Full-time.");
+			salary=Wage_per_hour*Full_time_hrs;
+			System.out.println("The Full time salary is "+salary);
+		}
+		else if (random_Check_1==parttime){
+			System.out.println("Employee is present and is Part-time.");
+			salary=Wage_per_hour*Part_time_hrs;
+			System.out.println("The Part time salary is "+salary);
+		}		
 	}
 	else{
 		System.out.println("Employee is absent");
