a75fdbe79e1ca4bc118f02ce6f3badc57806038b
unknown <rahulsurana37@gmail.com>
2020-06-22 15:31:09 +0530

    Added FUlltime Employee

--- Wage.java
+++ Wage.java
@@ -1,12 +1,15 @@
 class Wage{
 public static void main(String[] args) {
 	System.out.println("Welcome to JAVA exercise");
-
-int is_Present=1;
+	int is_Present=1;
+	int Wage_per_hour=20;
+	int Full_time=8;
+	int salary;
 	double random_Check=Math.floor(Math.random() * 10) % 2;
 	if (random_Check==is_Present) {
 		System.out.println("Employee is present");
-		
+		salary=Wage_per_hour*Full_time;
+		System.out.println(salary);
 	}
 	else{
 		System.out.println("Employee is absent");
