96891cb32eb4af0cb96ea62d312e691117b3feb7
unknown <rahulsurana37@gmail.com>
2020-06-26 14:53:44 +0530

    Added other companies

--- Wage.java
+++ Wage.java
@@ -1,45 +1,34 @@
 class Wage{
-	public int Emp_computation(){
-	int Wage_per_hour=20;
-	int total_working_days=20;
-	int total_working_hours=0;
-	int salary=0;
-	int hours=0;
-	int total_salary=0;
-	double random_Check=Math.floor(Math.random() * 10) % 2;
+	public static final int Is_Parttime=1;
+	public static final int Is_Fulltime=2;
 	
-	for (int i=0;i<total_working_days;i++) {
-		if(total_working_hours<100) {
-		double random_Check_1=Math.floor(Math.random() * 10) % 3;
-		int random=(int)random_Check_1;
-			switch(random){
-				case 1:
-				hours=8;
-				salary=Wage_per_hour*hours;
+		public static int employee_comp(String company, int wagePerHr, int number_of_working_days, int max_hrs_in_month){
+		int hrs=0, total_hrs=0, totalWorkingDays=0;
+		while (total_hrs <= max_hrs_in_month && totalWorkingDays < number_of_working_days){
+		totalWorkingDays++;
+		double random_Check_2=Math.floor(Math.random() * 10) % 3;
+		int random=(int)random_Check_2;	
+		switch(random){
+				case Is_Parttime:
+				hrs=4;
 				break;
-				case 2:
-			    hours=4;
-			    salary=Wage_per_hour*hours;
+				case Is_Fulltime:
+				hrs=8;
 				break;
 				default:
-				hours=0;
-				salary=0;
-				break;
+				hrs=0;
 			}
-			
-			total_salary=total_salary+salary;
-			total_working_hours=total_working_hours+hours;
+			total_hrs+=hrs;
+			System.out.println("Day: " + totalWorkingDays + " Emp Hours: " +hrs);
 		}
-		
-	}
-	System.out.println("Total working hours till now: "+total_working_hours);
-	return total_salary;
-}
-
+		int totalEmpWage=total_hrs*wagePerHr;
+		System.out.println("Total Emp wage for Company: " +company+" is: "+totalEmpWage);
+		return totalEmpWage;
+		}
 
 public static void main(String[] args) {
-	System.out.println("Welcome to JAVA exercise");
-	Wage abc= new Wage();
-	System.out.println("Total Salary: "+abc.Emp_computation());
+	employee_comp("Dmart", 20, 20, 100);
+	employee_comp("Reliance", 10, 14, 60);
+
 }
 }
